b0c3ca2fd93a778af945a6f64605bad049ccb8c4
Willian Morais Silva <wil.h2or@hotmail.com>
2021-04-23 23:46:04 -0300

    Update freightMutator.js

--- Cursos/JS/TwilioQuest/freightMutator.js
+++ Cursos/JS/TwilioQuest/freightMutator.js
@@ -1,2 +1,11 @@
+var carga = ['apples', 'ray guns', 'oranges', ]
+var itensMutantes = mutate(carga)
 
+function mutate(carga) {
+    
+    var arr = carga.map(item => item.toUpperCase())
 
+    return arr
+}
+
+console.log('Carga mutante: ' + itensMutantes)
