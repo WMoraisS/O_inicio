56c73d5936bf756b94cd07953fa67d3652feafb6
Willian Morais Silva <57778029+WMoraisS@users.noreply.github.com>
2021-07-29 20:44:34 -0300

    Include exemple

--- Projeto Extensão/content.js
+++ Projeto Extensão/content.js
@@ -1,1 +1,5 @@
-alert("Hi, I'm here!");
+// CÓDIGO SAP: document.getElementsByName("comp[ XXXXX ][composicao_codigo]")[0].value
+// QUANTIDADE: document.getElementsByName("comp[ XXXXX ][composicao_qtd]")[0].value
+
+var qtdItens = document.getElementsByClassName("list-group list-group-flush listaComponentes")[2].getElementsByClassName("row list-group-item").length
+alert(x)
