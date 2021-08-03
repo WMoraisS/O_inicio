45e3a00163aa5331a7a27f3d7fd533f72da6d408
Willian Morais Silva <57778029+WMoraisS@users.noreply.github.com>
2021-08-02 23:10:09 -0300

    B.O.Mv1

--- Projeto Extensão/content.js
+++ Projeto Extensão/content.js
@@ -1,5 +1,23 @@
 // CÓDIGO SAP: document.getElementsByName("comp[ XXXXX ][composicao_codigo]")[0].value
 // QUANTIDADE: document.getElementsByName("comp[ XXXXX ][composicao_qtd]")[0].value
 
-var qtdItens = document.getElementsByClassName("list-group list-group-flush listaComponentes")[2].getElementsByClassName("row list-group-item").length
-alert(x)
+var x = document.getElementsByClassName("list-group list-group-flush listaComponentes")[2].getElementsByClassName("row list-group-item")
+var codSAP
+var qtd
+var table = ""
+for (var i = 0; i < x.length; i++){
+    var html = ""
+    codSAP = x[i].getElementsByClassName("hide")[0].getElementsByTagName("input")[2].value
+    qtd = x[i].getElementsByClassName("hide")[0].getElementsByTagName("input")[3].value
+    html += '<table><tr><td>'+ codSAP + " " + qtd +'</td></tr></table>'
+    table = table + html
+}
+
+if (document.querySelector(".modal-title").text = "Editar solicitação para cadastro de lista técnica (composição)") {
+    
+  
+        //var local = document.getElementsByClassName("row")[234]
+        var local = document.getElementsByClassName('col-sm-12')[89]
+        local.innerHTML = local.innerHTML + table
+        
+}
