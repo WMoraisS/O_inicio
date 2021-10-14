75f00ef8869f87ba4984f63c07fbcf7b7ae2f22a
Willian Morais Silva <57778029+WMoraisS@users.noreply.github.com>
2021-10-13 21:34:07 -0300

    upDate13.10

---
+++ Cursos/Python/Cisco Academy/funcaoInput.py
@@ -0,0 +1,34 @@
+# CTRL + / ou CTRL + ; para adicionar comentários em um bloco de linhas
+## FUNÇÃO INPUT - Como falar com um computador
+# 2.6.1.1
+
+# print("Digite qualquer coisa: ")
+# anything = input()
+# print("Hmmm... ", anything, " ......really???")
+# #--------------------------------------------------------------------
+# anything = input("Digite qualquer coisa: ")
+# print("Hmmm... ", anything, " ......really???")
+
+# 2.6.1.2
+# anything = input("Enter a number: ")
+# something = float(anything) ** 2.0
+# print(anything, "to the power of 2 is", something)
+# #----------------------------------------------------------------------
+# anything = float(input("Enter a number: "))
+# something = anything ** 2.0
+# print(anything, "to the power of 2 is", something)
+
+# 2.6.1.6 - Operadores de String
+
+
+
+
+
+
+
+
+
+
+
+
+
