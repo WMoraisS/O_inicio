5dc8443f9fed61e9ef10964092b0038b99765c51
Willian Morais Silva <57778029+WMoraisS@users.noreply.github.com>
2021-10-23 20:26:38 -0300

    Update listas.py

--- Cursos/Python/Cisco Academy/listas.py
+++ Cursos/Python/Cisco Academy/listas.py
@@ -1,1 +1,38 @@
-# 3.4.1.1 - Listas
+# 3.4.1.11 - Listas
+
+# # step 1
+# beatles = []
+# print("Step 1:", beatles)
+
+# # step 2
+# beatles.append("John Lennon")
+# beatles.append("Paul McCartney")
+# beatles.append("George Harrison")
+# print("Step 2:", beatles)
+    
+# # step 3
+# print("...Head noice... ~Stu Sutcliffe~|~Pete Best~")
+# for i in range(2):
+#     beatle = input("Digite o nome dos outros integrantes da banda: ")
+#     beatles.append(beatle)
+
+# print("Step 3:", beatles)
+
+# # step 4
+# i = 1
+# while i != len(beatles):
+#     if beatles[i] == "Stu Sutcliffe" or beatles[i] == "Pete Best": 
+#         del(beatles[i])
+#         i -= 1
+#     i += 1
+
+# print("Step 4:", beatles)
+
+# # step 5
+# beatles.insert(0, "Ringo Starr")
+# print("Step 5:", beatles)
+
+
+# # testing list legth
+# print("The Fab", len(beatles))
+
