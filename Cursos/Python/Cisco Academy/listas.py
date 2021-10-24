f6190e518a8c79f220a27acdbee2c4a64944e238
Willian Morais Silva <57778029+WMoraisS@users.noreply.github.com>
2021-10-24 18:45:04 -0300

    ok

--- Cursos/Python/Cisco Academy/listas.py
+++ Cursos/Python/Cisco Academy/listas.py
@@ -36,3 +36,58 @@
 # # testing list legth
 # print("The Fab", len(beatles))
 
+# my_old_list = [8,10,6,2,4]
+# my_list = [8,10,6,2,4]
+# count = True
+# while count == True:
+#     count = False
+#     for i in range(len(my_list) - 1):
+#         if my_list[i] > my_list[i + 1]:
+#             my_list[i], my_list[i + 1] = my_list[i +1], my_list[i]
+#             print(my_list)
+#             count = True
+    
+# print(my_list)
+
+# my_old_list.sort() # Ordena a lista em ordem Crescente
+# print("Usando o .sort", my_old_list)
+# my_old_list.reverse() # Ordena a lista em ordem Decrescente
+# print("Usando o .reverse", my_old_list)
+
+#A VIDA INTERNA DAS LISTAS
+# list_1 = [1]
+# list_2 = list_1
+# list_1[0] = 2
+# print(list_2)
+
+# Ao declarar 'list_2 = list_1' será copiado somente o nome da lista, herdando suas informações e alterações.
+# As listas (e muitas outras entidades complexas de Python) são armazenadas de formas diferentes das variáveis ordinárias (escalares).
+# -O nome de uma variável ordinária é o nome do seu conteúdo;
+# -O nome de uma lista é o nome de um local de memória onde a lista é armazenada.
+
+
+# >>   SLICE   << - Copia o conteúdo da lista, não somente seu nome
+# Uma slice é um elemento da sintaxe Python que lhe permite fazer uma cópia completamente nova de uma lista ou partes de uma lista.
+# list_1 = [1]
+# list_2 = list_1[:] # Cria uma cópia da lista inteira.
+# list_1[0] = 2
+# print(list_2)
+#
+# my_list = [10, 8, 6, 4, 2]
+# new_list = my_list[1:3] # Cria uma cópia parcial da lista. Sintaxe: my_list[start:end] (end - 1)
+# print(new_list)
+#
+# start é o index do primeiro elemento incluído no slice;
+# end é o index do primeiro elemento não incluído no slice.
+
+# del my_list[:] # Elimina todos os elementos da lista
+# del my_list[]  # Elimina a lista em si.
+
+
+# Os loops 'in' e 'not in' operadores
+# O Python oferece dois operadores muito poderosos, capazes de olhar através da lista a fim de verificar se um valor específico 
+# está ou não armazenado dentro da lista. Retorna resultado booleano.
+# elem in my_list
+# elem not in my_list
+
+###### 3.7.1.1
